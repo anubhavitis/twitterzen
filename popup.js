6e505a251eb86f9ba636626442784a609b579eaa
anubhav.s <anubhav.s@coinswitch.co>
2023-01-28 18:43:47 +0530

    merge issues fixed

--- popup.js
+++ popup.js
@@ -20,6 +20,16 @@ myButton.onclick = () => {
     );
   });
 
+  ChangeButton();
+
+  // Reload
+  chrome.tabs.getSelected(null, function (tab) {
+    // calling external js file directly instead of on load
+    chrome.tabs.executeScript(tab.id, { file: './custom_js_script.js' });
+  });
+};
+
+function ChangeButton() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
 
@@ -29,15 +39,20 @@ myButton.onclick = () => {
       (data) => {
         enabled = data[0];
         console.log('enabled:', enabled);
-        myButton.textContent = enabled == 'true' ? 'Disable' : 'Enable';
+        if (enabled === "true") {
+          myButton.textContent = "Disable";
+          myButton.classList.remove('enable-btn')
+          myButton.classList.add('disable-btn')
+        } else {
+          myButton.textContent = "Enable";
+          myButton.classList.remove('disable-btn')
+          myButton.classList.add('enable-btn')
+        }
+        // myButton.textContent = enabled == 'true' ? 'Disable' : 'Enable';
         console.log('button text:', myButton.textContent);
       }
     );
   });
+}
 
-  // Reload
-  chrome.tabs.getSelected(null, function (tab) {
-    // calling external js file directly instead of on load
-    chrome.tabs.executeScript(tab.id, { file: './custom_js_script.js' });
-  });
-};
+window.addEventListener("load", ChangeButton)
