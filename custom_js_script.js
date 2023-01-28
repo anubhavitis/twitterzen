e399bb84062ea642224634fe66e5f8ad1ef64789
Jagdish Patil <patil.jagdish222@gmail.com>
2023-01-28 16:56:59 +0530

    show/hide impression stats without reload

--- custom_js_script.js
+++ custom_js_script.js
@@ -1,7 +1,7 @@
-console.log('custom_js_script.js called')
-var enabled = "false" //default
-var impression_display = "block" //default
-  
+console.log('custom_js_script.js called');
+var enabled = window.localStorage.getItem('enabled'); //default
+
+var impression_display = enabled == 'true' ? 'none' : 'block'; //default
 function getNthChild(father, n) {
   var child = father;
   for (var i = 0; i < n; i += 1) {
@@ -10,7 +10,6 @@ function getNthChild(father, n) {
   return child;
 }
 
-
 function processTweet(tweet) {
   var child = getNthChild(tweet, 3);
   var body = child.children.item(1).children.item(1).children.item(1);
@@ -18,42 +17,34 @@ function processTweet(tweet) {
   var stats = footer.children.item(3);
 
   if (stats) {
+    debugger;
     stats.style.display = impression_display;
-    tweet.classList.add("flag");
+    tweet.classList.add('flag');
   }
 }
-
 
 function mywork() {
-  var obj = document.getElementsByTagName("article");
+  var obj = document.getElementsByTagName('article');
+  // removed flag condition because it used to get only recent tweets
   for (var i = 0; i < obj.length; i += 1) {
-    if (!obj[i].classList.contains("flag")) {
-      processTweet(obj[i]);
-    }
+    processTweet(obj[i]);
   }
 }
 
-
 function delayAndExecute() {
-  console.log("executing script:", enabled)
+  console.log('executing script:', enabled);
   setTimeout(function () {
     mywork();
   }, 2500);
 }
 
-window.addEventListener("scroll", function() {
-  delayAndExecute()
-}, false);
+window.addEventListener(
+  'scroll',
+  function () {
+    delayAndExecute();
+  },
+  false
+);
 
-window.onload = function () {
-  enabled = window.localStorage.getItem('enabled');
-  impression_display = (enabled == "true") ? "none" : "block";
-  console.log("script enabled:", enabled, "hence:", impression_display)
-  delayAndExecute()
-}
-
-
-// IE
-// window.attachEvent("onload", function () {
-//   delayAndExecute();
-// });
+console.log('script enabled:', enabled, 'hence:', impression_display);
+mywork();
--- popup.js
+++ popup.js
@@ -1,44 +1,43 @@
-var enabled = "false"; //disabled by default
-var storageKey = "enabled"
-var myButton = document.getElementById("toggle");
+var enabled = 'false'; //disabled by default
+var storageKey = 'enabled';
+var myButton = document.getElementById('toggle');
 
 myButton.onclick = () => {
-  console.log('Clicked here')
-  enabled = (enabled=="true") ? "false" : "true";
-  chrome.storage.sync.set({ 'enabled': enabled })
+  console.log('Clicked here');
+  enabled = enabled == 'true' ? 'false' : 'true';
+  chrome.storage.sync.set({ enabled: enabled });
 
   // Update to localStorage
-  chrome.tabs.query({ "active": true, "currentWindow": true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
-    
-    chrome.tabs.executeScript(tab.id, { code: `localStorage.setItem("enabled", ${enabled});` }, (data) => {
-      console.log("update cache data:", data)
-    })
+
+    chrome.tabs.executeScript(
+      tab.id,
+      { code: `localStorage.setItem("enabled", ${enabled});` },
+      (data) => {
+        console.log('update cache data:', data);
+      }
+    );
+  });
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+
+    chrome.tabs.executeScript(
+      tab.id,
+      { code: `localStorage.getItem("enabled");` },
+      (data) => {
+        enabled = data[0];
+        console.log('enabled:', enabled);
+        myButton.textContent = enabled == 'true' ? 'Disable' : 'Enable';
+        console.log('button text:', myButton.textContent);
+      }
+    );
   });
 
   // Reload
   chrome.tabs.getSelected(null, function (tab) {
-    var code = 'window.location.reload();';
-    chrome.tabs.executeScript(tab.id, { code: code });
-    location.reload()
+    // calling external js file directly instead of on load
+    chrome.tabs.executeScript(tab.id, { file: './custom_js_script.js' });
   });
 };
-
-
-
-window.onload = function () {
-  console.log('hello, world')
-
-  chrome.tabs.query({ "active": true, "currentWindow": true }, function (tabs) {
-    var tab = tabs[0];
-    
-    chrome.tabs.executeScript(tab.id, { code: `localStorage.getItem("enabled");` }, (data) => {
-      enabled = data[0];
-      console.log('enabled:', enabled)
-      myButton.textContent = (enabled=="true") ? "Disable" : "Enable";
-      console.log("button text:", myButton.textContent)
-    });
-  });
-
-};
-
